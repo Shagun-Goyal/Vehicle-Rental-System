6b6b35f156916602e16458474891117f6509dd79
Shagun Goyal <shagungoyal@Shagun-MacBook-Air.local>
2025-09-07 21:30:07 +0530

    Updating index file to display

--- Index.cpp
+++ Index.cpp
@@ -1,6 +1,29 @@
 #include <iostream>
 using namespace std;
 
+class access {
+    public:
+    char i;
+    double y;
+};
 int main (){
-    cout << 'Hello World ' << endl;
+    for(int i = 0; i < 64; i++){
+        if(i == 32)
+            cout << endl;
+        cout << "-" ;
+    }
+    cout << endl;
+    for(int i = 0; i < 6; i++){
+        cout << " ";
+        if (i == 5)
+            cout << "Vehicle Rental System" << endl;
+    }
+    for(int i = 0; i < 64; i++){
+        if(i == 32)
+            cout << endl;
+        cout << "-" ;
+    }
+    access Admin, Customer;
+    string registeredAs;
+
 }
